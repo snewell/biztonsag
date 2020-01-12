e2815fc76c62542390e19015711eefd1482d24eb
Stephen Newell <sjnewell@gmail.com>
2020-01-11 17:51:03 -0700

    Expand temperature conversion example

--- examples/temp.cpp
+++ examples/temp.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <iostream>
+#include <type_traits>
 
 #include <biztonsag/comparable.hpp>
 
@@ -8,29 +9,82 @@ namespace
     struct FahrenheitTag
     {
     };
+
     struct CelsiusTag
+    {
+    };
+
+    struct KelvinTag
     {
     };
 
     using Fahrenheit = btshn::Comparable<double, FahrenheitTag>;
     using Celsius = btshn::Comparable<double, CelsiusTag>;
+    using Kelvin = btshn::Comparable<double, KelvinTag>;
 
-    Fahrenheit convert(Celsius temp) noexcept
+    namespace detail
     {
-        return Fahrenheit{static_cast<double const &>(temp) * 1.8 + 32};
-    }
+        template <typename TO, typename FROM>
+        TO convert(FROM temp) noexcept;
 
-    Celsius convert(Fahrenheit temp) noexcept
+        template <>
+        Kelvin convert<Kelvin, Celsius>(Celsius temp) noexcept
+        {
+            return Kelvin{(*temp) + 273.15};
+        }
+
+        template <>
+        Celsius convert<Celsius, Kelvin>(Kelvin temp) noexcept
+        {
+            return Celsius{(*temp) - 273.15};
+        }
+
+        template <>
+        Celsius convert<Celsius, Fahrenheit>(Fahrenheit temp) noexcept
+        {
+            return Celsius{((*temp) - 32) / 1.8};
+        }
+
+        template <>
+        Fahrenheit convert<Fahrenheit, Celsius>(Celsius temp) noexcept
+        {
+            return Fahrenheit{(*temp) * 1.8 + 32};
+        }
+
+        template <>
+        Fahrenheit convert<Fahrenheit, Kelvin>(Kelvin temp) noexcept
+        {
+            return convert<Fahrenheit>(convert<Celsius>(temp));
+        }
+
+        template <>
+        Kelvin convert<Kelvin, Fahrenheit>(Fahrenheit temp) noexcept
+        {
+            return convert<Kelvin>(convert<Celsius>(temp));
+        }
+    } // namespace detail
+
+    template <typename TO, typename FROM>
+    TO convert(FROM temp) noexcept
     {
-        return Celsius{(static_cast<double const &>(temp) - 32) / 1.8};
+        if constexpr(std::is_same<TO, FROM>::value)
+        {
+            return temp;
+        }
+        else
+        {
+            return detail::convert<TO>(temp);
+        }
     }
 
     template <typename TEMP>
-    void print(char first, char second, char const * num)
+    void print(char const * num)
     {
-        std::cout << num << first << " = "
-                  << static_cast<double const &>(convert(TEMP{std::atof(num)}))
-                  << second << '\n';
+        TEMP const input{std::atof(num)};
+
+        std::cout << *(convert<Kelvin>(input)) << "K\t"
+                  << *(convert<Celsius>(input)) << "C\t"
+                  << *(convert<Fahrenheit>(input)) << "F\n";
     }
 } // namespace
 
@@ -43,12 +97,17 @@ int main(int argc, char ** argv)
         {
         case 'c':
         case 'C':
-            print<Celsius>('C', 'F', num);
+            print<Celsius>(num);
             break;
 
         case 'f':
         case 'F':
-            print<Fahrenheit>('F', 'C', num);
+            print<Fahrenheit>(num);
+            break;
+
+        case 'k':
+        case 'K':
+            print<Kelvin>(num);
             break;
         }
     }
