624e132c3f8d44f4d3dccef83d6642c58f1ed432
Stephen Newell <sjnewell@gmail.com>
2020-01-18 17:19:59 -0700

    Add more explicit tests for construction

--- test/ctor_test.cpp
+++ test/ctor_test.cpp
@@ -44,3 +44,69 @@ TEST(Constructor, non_initializer_list) // NOLINT
 
     ASSERT_EQ(size, i->size());
 }
+
+namespace
+{
+    enum class ConstructorType
+    {
+        default_constructor,
+        args_constructor,
+        move_constructor,
+        initializer_list_constructor
+    };
+
+    struct ComplexBase
+    {
+        ComplexBase()
+          : m_type{ConstructorType::default_constructor}
+        {
+        }
+
+        ComplexBase(int a, int b)
+          : m_type{ConstructorType::args_constructor}
+        {
+            (void)a;
+            (void)b;
+        }
+
+        ComplexBase(std::initializer_list<int> init_list)
+          : m_type{ConstructorType::initializer_list_constructor}
+        {
+            (void)init_list;
+        }
+
+        ComplexBase(ComplexBase && c)
+          : m_type{ConstructorType::move_constructor}
+        {
+            (void)c;
+        }
+
+        ConstructorType const m_type;
+    };
+
+    BTSHN_MAKE_WRAPPED(Complex, ComplexBase);
+} // namespace
+
+TEST(Constructor, multi_default)
+{
+    Complex c{};
+    ASSERT_EQ(ConstructorType::default_constructor, c->m_type);
+}
+
+TEST(Constructor, multi_args)
+{
+    Complex c(0, 0);
+    ASSERT_EQ(ConstructorType::args_constructor, c->m_type);
+}
+
+TEST(Constructor, multi_initializer_list)
+{
+    Complex c{0, 0};
+    ASSERT_EQ(ConstructorType::initializer_list_constructor, c->m_type);
+}
+
+TEST(Constructor, multi_move)
+{
+    Complex c{ComplexBase{}};
+    ASSERT_EQ(ConstructorType::move_constructor, c->m_type);
+}
